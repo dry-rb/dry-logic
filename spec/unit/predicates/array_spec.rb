bf4a016c7ab06e65f7dc63d5a857e92c78be280a
Ralf Schmitz Bongiolo <ralfsb2008@gmail.com>
2016-05-27 16:08:02 -0400

    added spec for predicate :array

---
+++ spec/unit/predicates/array_spec.rb
@@ -0,0 +1,41 @@
+require 'dry/logic/predicates'
+
+RSpec.describe Dry::Logic::Predicates do
+  describe '#array?' do
+    let(:predicate_name) { :array? }
+
+    context 'when value is an array' do
+      let(:arguments_list) do
+        [
+          [ [] ],
+          [ ['other', 'array'] ],
+          [ [123, 'really', :blah] ],
+          [ Array.new ],
+          [ [nil] ],
+          [ [false] ],
+          [ [true] ]
+        ]
+      end
+
+      it_behaves_like 'a passing predicate'
+    end
+
+    context 'when value is not an array' do
+      let(:arguments_list) do
+        [
+          [''],
+          [{}],
+          [nil],
+          [:symbol],
+          [String],
+          [1],
+          [1.0],
+          [true],
+          [Hash.new]
+        ]
+      end
+
+      it_behaves_like 'a failing predicate'
+    end
+  end
+end
