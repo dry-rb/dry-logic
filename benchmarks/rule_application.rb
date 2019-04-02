c7ef3f64622907014a1ef334053cbe5cc33fef15
Nikita Shilnikov <fg@flashgordon.ru>
2019-04-02 20:58:00 +0300

    Reduce allocations for failure cases by passing an empty block [skip ci]
    Calculating -------------------------------------
    type check - success      7.955M (± 6.2%) i/s -     39.758M in   5.020519s
    type check - failure      5.123M (± 4.1%) i/s -     25.636M in   5.013768s
     int check - success      7.992M (± 5.8%) i/s -     40.150M in   5.043370s
     int check - failure      5.273M (± 4.3%) i/s -     26.352M in   5.008398s
     key check - success      7.396M (± 5.1%) i/s -     36.906M in   5.004942s
     key check - failure      5.965M (± 3.5%) i/s -     30.024M in   5.040508s
    comparison - success      7.605M (± 3.8%) i/s -     38.141M in   5.023874s
    comparison - failure      5.944M (± 5.0%) i/s -     29.668M in   5.005995s

--- benchmarks/rule_application.rb
+++ benchmarks/rule_application.rb
@@ -15,14 +15,14 @@ without_user = {}
 comparison = Dry::Logic::Rule.build(predicates[:gteq?]).curry(18)
 
 Benchmark.ips do |x|
-  x.report("type check - success") { type_check.(0) }
-  x.report("type check - failure") { type_check.('0') }
-  x.report("int check - success") { int_check.(0) }
-  x.report("int check - failure") { int_check.('0') }
-  x.report("key check - success") { key_check.(with_user) }
-  x.report("key check - failure") { key_check.(without_user) }
-  x.report("comparison - success") { comparison.(20) }
-  x.report("comparison - failure") { comparison.(17) }
+  x.report("type check - success") { type_check.(0) {} }
+  x.report("type check - failure") { type_check.('0') {} }
+  x.report("int check - success") { int_check.(0) {} }
+  x.report("int check - failure") { int_check.('0') {} }
+  x.report("key check - success") { key_check.(with_user) {} }
+  x.report("key check - failure") { key_check.(without_user) {} }
+  x.report("comparison - success") { comparison.(20) {} }
+  x.report("comparison - failure") { comparison.(17) {} }
 
   x.compare!
 end
