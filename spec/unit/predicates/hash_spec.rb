b980b534d6af391c206a93e082dbb9c1f979fbc5
Nikita Shilnikov <fg@flashgordon.ru>
2019-07-04 00:15:45 +0300

    Add specs for hash?

---
+++ spec/unit/predicates/hash_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'dry/logic/predicates'
+
+RSpec.describe Dry::Logic::Predicates do
+  describe '#hash?' do
+    let(:predicate_name) { :hash? }
+
+    context 'when value is a hash' do
+      let(:arguments_list) do
+        [
+          [{}],
+          [foo: :bar],
+          [Hash.new]
+        ]
+      end
+
+      it_behaves_like 'a passing predicate'
+    end
+
+    context 'when value is not a hash' do
+      let(:arguments_list) do
+        [
+          [''],
+          [[]],
+          [nil],
+          [:symbol],
+          [String],
+          [1],
+          [1.0],
+          [true],
+          [Array.new],
+          [Hash]
+        ]
+      end
+
+      it_behaves_like 'a failing predicate'
+    end
+  end
+end
