f16c1096fa13ee9e996ac1f53e260e38b45a85c7
Thomas Himbert <thomas@company-mood.com>
2019-09-11 12:45:49 +0200

    Remove fit from specs

--- spec/unit/rule_spec.rb
+++ spec/unit/rule_spec.rb
@@ -204,7 +204,7 @@ RSpec.describe Dry::Logic::Rule do
     describe 'constants' do
       let(:options) { { args: [], arity: 0 } }
 
-      fit 'accepts variable number of arguments' do
+      it 'accepts variable number of arguments' do
         expect(rule.method(:call).arity).to be(-1)
         expect(rule.method(:[]).arity).to be(-1)
       end
