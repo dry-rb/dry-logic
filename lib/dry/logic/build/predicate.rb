820aab1a80da956bc9b714f73a3f3f1bb5756b32
Linus Oleander <linus@oleander.io>
2021-02-01 19:05:02 +0100

    Document Build::Predicate.predicate

--- lib/dry/logic/build/predicate.rb
+++ lib/dry/logic/build/predicate.rb
@@ -7,6 +7,32 @@ module Dry
   module Logic
     module Build
       class Predicate < Base
+        #
+        # Defines a user defined predicate
+        #
+        # @name [Symbol] A predicate name
+        # @block [Proc] Block containing the predicate logic
+        # @example Checks input is equals to 10
+        #   predicate(:ten?) do |input|
+        #     input == 10
+        #   end
+        #
+        #   is_ten = predicates[:ten?]
+        #
+        #   is_ten.call(10).success? # => true
+        #   is_ten.call(11).success? # => false
+        #
+        def predicate(name, &block)
+          # Remove the existing predicate defined by user
+          # Without this a warning is shown similar to:
+          #   warning: method redefined; discarding old {name}
+          if respond_to_missing?(name)
+            predicates.singleton_class.undef_method(name)
+          end
+
+          predicates[:predicate].call(name, &block)
+        end
+
         def method_missing(method, *args, **kwargs, &block)
           super unless respond_to_missing?(method)
           super if Kernel.block_given?
@@ -17,17 +43,6 @@ module Dry
 
         def respond_to_missing?(method, *)
           predicates.methods.include?(method)
-        end
-
-        def predicate(name, &block)
-          # Remove the existing predicate defined by user
-          # Without this a warning is shown similar to:
-          #   warning: method redefined; discarding old {name}
-          if respond_to_missing?(name)
-            predicates.singleton_class.undef_method(name)
-          end
-
-          predicates[:predicate].call(name, &block)
         end
 
         private
