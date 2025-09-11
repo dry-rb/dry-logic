2599ee2411a8664dbdd95909a73261319934d00b
Adam Lassek <adam@doubleprime.net>
2025-09-10 23:47:28 -0500

    Adopt Hanakai rubocop styles

--- spec/integration/rule_spec.rb
+++ spec/integration/rule_spec.rb
@@ -46,7 +46,6 @@ RSpec.describe "Rules" do
   end
 
   specify "defining a rule with options" do
-    # rubocop:disable Style/SymbolProc
     # using &:empty? breaks the spec
     rule = Dry::Logic::Rule(id: :empty?) { |value| value.empty? }
     # rubocop:enable Style/SymbolProc
